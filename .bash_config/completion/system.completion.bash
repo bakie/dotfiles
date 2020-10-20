0693bda79381177aa5d2a24d70c1d5506617a462
Bakie <bakelants.developer@gmail.com>
2020-10-20 05:57:55 +0200

    added system completion

---
+++ .bash_config/completion/system.completion.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [[ -f /etc/bash_completion ]]; then
+  source /etc/bash_completion
+fi
+
+if [[ -f /etc/profile.d/bash_completion.sh ]]; then
+  source /etc/profile.d/bash_completion.sh
+fi
+
+
+if [[ $(uname) = "Darwin" ]] && command -v brew &>/dev/null ; then
+  BREW_PREFIX=$(brew --prefix)
+
+  if [[ -f "$BREW_PREFIX"/etc/bash_completion ]]; then
+    source "$BREW_PREFIX"/etc/bash_completion
+  fi
+
+  if [[ -f "$BREW_PREFIX"/share/bash-completion/bash_completion ]]; then
+    source "$BREW_PREFIX"/share/bash-completion/bash_completion
+  fi
+fi
